767574435072dd2347bcd19c07699ce4925f5cda
urishav <mail.rishav.kr@gmail.com>
2021-11-09 19:08:35 +0530

    typos

--- python/python_lamda_map_filter_reduce.py
+++ python/python_lamda_map_filter_reduce.py
@@ -11,20 +11,21 @@ print("Lambda : {}".format(greater(10, 20)))
 
 '''
 Map :
-map(lambda func : iterable) 
+map(lambda func , iterable) 
 '''
 cube = list(map(lambda x : x**3, [1, 2, 3]))
 print("Map : {}".format(cube))
 
 '''
 Filter :
-filter(lambda func : iterable) 
+filter(lambda func , iterable) 
 '''
 list_answer = list(filter(lambda x : x % 5 ==0, [2,3,5,7,10,12,15]))
 print("Filter : {}".format(list_answer))
 
 '''
 Reduce :
+reduce(lambda func, iterable)
 '''
 reduce_out = reduce(lambda x,y : x + " : " + y, ["hi", "there", "this", "is", "a", "test" ])
 print("Reduce output : {}".format(reduce_out))
